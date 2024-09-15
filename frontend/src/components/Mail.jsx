32aebeaaea62c9b3d066211d504371d14ce5c33b
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-09-15 17:54:06 +0530

    Update Mail.jsx

--- frontend/src/components/Mail.jsx
+++ frontend/src/components/Mail.jsx
@@ -15,7 +15,7 @@ const Mail = () => {
     const params = useParams();
     const deleteHandler = async () => {
         try {
-            const res = await axios.delete(`http://localhost:4500/api/v1/email/${params.id}`,{withCredentials:true});
+            const res = await axios.delete(`https://gmail-clone-1-u2jb.onrender.com/api/v1/email/${params.id}`,{withCredentials:true});
             toast.success(res.data.message);
             navigate("/");
         } catch (error) {
