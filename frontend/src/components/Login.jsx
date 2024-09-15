d511c63dd68411c3bc88a38f0c9846f0536f3358
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-09-15 18:25:29 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
         e.preventDefault();
         console.log(input);
         try {
-            const res = await axios.post("https://gmail-clone-hpih.onrender.com/api/v1/user/login", input, {
+            const res = await axios.post(`https://gmail-clone-hpih.onrender.com/api/v1/user/login`, input, {
                 headers:{
                     'Content-Type':"application/json"
                 },
