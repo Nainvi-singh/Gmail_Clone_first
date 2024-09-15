8cc3f2c67f1fc0b073f6c3d1116a7f0c402bb822
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-09-15 17:54:26 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     const navigate = useNavigate();
     const logoutHandler = async () => {
         try {
-            const res = await axios.get('http://localhost:4500/api/v1/user/logout',{withCredentials:true});
+            const res = await axios.get('https://gmail-clone-1-u2jb.onrender.com/api/v1/user/logout',{withCredentials:true});
             console.log(res);
             toast.success(res.data.message);
             dispatch(setAuthUser(null));
