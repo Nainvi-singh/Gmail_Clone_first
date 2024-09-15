530119277dc1931aed1c7ca2f602a9b4cfe28b99
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-09-15 18:53:53 +0530

    Update isAuthenticated.js

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req,res,next) => {
     try {
+        console.log(token);
         const token = req.cookies.token;
        
         if(!token){
