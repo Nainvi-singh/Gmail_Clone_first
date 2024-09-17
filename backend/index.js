a8cfd0a0cdbabc92d7a8a5e9b55e0e6307828d6a
Vivek-27 <vivekvb@protonmail.com>
2024-09-17 22:51:32 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
-import express from "express"; // react style
-import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import userRoute from "./routes/user.route.js";
-import emailRoute from "./routes/email.route.js";
+import express from 'express'; // react style
+import dotenv from 'dotenv';
+import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import userRoute from './routes/user.route.js';
+import emailRoute from './routes/email.route.js';
 
 dotenv.config({});
 connectDB();
 const PORT = 4500;
 const app = express();
 
-app.use(cors({origin:`https://gmail-clone-2-qj4n.onrender.com`,credentials:true}));
-app.options('*',cors())
+// app.use(cors({origin:`https://gmail-clone-2-qj4n.onrender.com`,credentials:true}));
+// app.options('*',cors())
 // middleware
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 // const corsOptions = {
 //     origin:'http://localhost:5174',
@@ -25,9 +26,9 @@ app.use(cookieParser());
 // app.use(cors(corsOptions));
 
 // routes
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/email", emailRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/email', emailRoute);
 
-app.listen(PORT, ()=>{
-    console.log(`Server running at port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
--- frontend/src/hooks/useGetAllEmails.js
+++ frontend/src/hooks/useGetAllEmails.js
@@ -1,25 +1,23 @@
-import axios from "axios"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux";
-import { setEmails } from "../redux/appSlice";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setEmails } from '../redux/appSlice';
 
 const useGetAllEmails = () => {
-    const dispatch = useDispatch();
-    const {emails} = useSelector(store=>store.app);
-    useEffect(() => {
-        const fetchEmails = async () => {
-            try {
-
-                const res = await axios.get(`https://gmail-clone-hpih.onrender.com/api/v1/email/getallemails`, {
-                    withCredentials: true
-                });
-                dispatch(setEmails(res.data.emails));
-
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchEmails();
-    }, []);
+  const dispatch = useDispatch();
+  const { emails } = useSelector((store) => store.app);
+  useEffect(() => {
+    const fetchEmails = async () => {
+      try {
+        const res = await axios.get(`/api/v1/email/getallemails`, {
+          withCredentials: true
+        });
+        dispatch(setEmails(res.data.emails));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchEmails();
+  }, []);
 };
 export default useGetAllEmails;
